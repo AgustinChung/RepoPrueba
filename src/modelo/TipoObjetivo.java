5efc01dc2d761ff764abd5711beaa0914ecf84bd
mbanchero <mbanchero21@gmail.com>
2024-11-12 13:44:03 -0300

    Delete src directory

--- src/modelo/TipoObjetivo.java
+++
@@ -1,8 +0,0 @@
-package modelo;
-
-public enum TipoObjetivo {
-	DERROTAR_DRAGON_NORTE,
-	RECUPERAR_AMULETO,
-	ELIMINAR_ESPECTRO,
-	LIMPIAR_ALDEA
-}
--- src/modelo/TipoUbicacion.java
+++
@@ -1,8 +0,0 @@
-package modelo;
-
-public enum TipoUbicacion {
-	MONTANIAS_HELADAS,
-	BOSQUE_DE_LOS_SUSURROS,
-	PANTANO_OSCURO,
-	ALDEA_DE_LOS_SIRITH
-}
